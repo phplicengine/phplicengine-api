8eef79f3d7487d3c70bfd9e9c628d042f003d23a
PHPLicengine <phplicengine@gmail.com>
2015-08-02 13:16:41 +0430

    Create Client.php

---
+++ lib/PHPLicengine/Service/Client.php
@@ -0,0 +1,32 @@
+<?php
+
+// Client.php
+#################################################
+## PHPLicengine
+##
+## Homepage: http://www.phplicengine.com
+#################################################
+## Copyright 2009-{current_year} PHPLicengine
+## 
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##    http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+#################################################
+
+namespace PHPLicengine\Service;
+
+class Client extends \PHPLicengine\Api (
+ 
+      public function getClientById ($base_url, $clientId) {
+             return $this->_call($base_url . '/api/client/' . $clientId);
+      }
+      
+)
