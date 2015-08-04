fbad0c74dbd83e30f6f3b625172bb16af88eadd9
PHPLicengine <phplicengine@gmail.com>
2015-08-04 14:04:56 +0430

    Update Api.php

--- lib/PHPLicengine/Api/Api.php
+++ lib/PHPLicengine/Api/Api.php
@@ -32,71 +32,6 @@ class Api {
            protected $curlErrno = false;
            protected $curlError = false;
 
-           protected $reasonPhrases = array(
-                     // INFORMATIONAL CODES
-                     100 => 'Continue',
-                     101 => 'Switching Protocols',
-                     102 => 'Processing',
-                     // SUCCESS CODES
-                     200 => 'OK',
-                     201 => 'Created',
-                     202 => 'Accepted',
-                     203 => 'Non-Authoritative Information',
-                     204 => 'No Content',
-                     205 => 'Reset Content',
-                     206 => 'Partial Content',
-                     207 => 'Multi-status',
-                     208 => 'Already Reported',
-                     // REDIRECTION CODES
-                     300 => 'Multiple Choices',
-                     301 => 'Moved Permanently',
-                     302 => 'Found',
-                     303 => 'See Other',
-                     304 => 'Not Modified',
-                     305 => 'Use Proxy',
-                     306 => 'Switch Proxy', // Deprecated
-                     307 => 'Temporary Redirect',
-                     // CLIENT ERROR
-                     400 => 'Bad Request',
-                     401 => 'Unauthorized',
-                     402 => 'Payment Required',
-                     403 => 'Forbidden',
-                     404 => 'Not Found',
-                     405 => 'Method Not Allowed',
-                     406 => 'Not Acceptable',
-                     407 => 'Proxy Authentication Required',
-                     408 => 'Request Time-out',
-                     409 => 'Conflict',
-                     410 => 'Gone',
-                     411 => 'Length Required',
-                     412 => 'Precondition Failed',
-                     413 => 'Request Entity Too Large',
-                     414 => 'Request-URI Too Large',
-                     415 => 'Unsupported Media Type',
-                     416 => 'Requested range not satisfiable',
-                     417 => 'Expectation Failed',
-                     418 => 'I\'m a teapot',
-                     422 => 'Unprocessable Entity',
-                     423 => 'Locked',
-                     424 => 'Failed Dependency',
-                     425 => 'Unordered Collection',
-                     426 => 'Upgrade Required',
-                     428 => 'Precondition Required',
-                     429 => 'Too Many Requests',
-                     431 => 'Request Header Fields Too Large',
-                     // SERVER ERROR
-                     500 => 'Internal Server Error',
-                     501 => 'Not Implemented',
-                     502 => 'Bad Gateway',
-                     503 => 'Service Unavailable',
-                     504 => 'Gateway Time-out',
-                     505 => 'HTTP Version not supported',
-                     506 => 'Variant Also Negotiates',
-                     507 => 'Insufficient Storage',
-                     508 => 'Loop Detected',
-                     511 => 'Network Authentication Required'
-           );
-
            public function __construct($api_key = null) 
            { 
                   if (!function_exists('curl_init')) 
@@ -174,7 +109,7 @@ class Api {
                   }
                   $this->curlInfo = curl_getinfo($ch);
                   curl_close($ch);
-                  return new Result($this->_getBody(), $this->_getHeaders());
+                  return new Result($this->_getBody(), $this->_getHeaders(), $this->curlInfo);
            }
 
            private function _parseHeaders($raw_headers) 
@@ -244,56 +179,6 @@ class Api {
            public function put($url, $params = "", $headers = "") 
            {
                   return $this->_call($url, $params, $headers, $method = "PUT")      
-           }
-
-           public function getResponseCode()
-           {
-                  return $this->curlInfo['http_code'];
-           }
-
-           public function getReasonPhrase()
-           {
-                  return $this->reasonPhrases[$this->curlInfo['http_code']];
-           }
-
-           public function getContentType()
-           {
-                  return $this->curlInfo['content_type'];
-           }
-
-           public function isOk()
-           {
-                   return ($this->curlInfo['http_code'] === 200);
-           }
-
-           public function isSuccess()
-           {
-                  return (200 <= $this->curlInfo['http_code'] && 300 > $this->curlInfo['http_code']);
-           }
-
-           public function isNotFound()
-           {
-                  return ($this->curlInfo['http_code'] === 404);
-           }
-
-           public function isInformational()
-           {
-                  return ($this->curlInfo['http_code'] >= 100 && $this->curlInfo['http_code'] < 200);
-           }
-
-           public function isRedirect()
-           {
-                  return (300 <= $this->curlInfo['http_code'] && 400 > $this->curlInfo['http_code']);
-           }
-
-           public function isClientError()
-           {
-                  return ($this->curlInfo['http_code'] < 500 && $this->curlInfo['http_code'] >= 400);
-           }
-
-           public function isServerError()
-           {
-                  return (500 <= $this->curlInfo['http_code'] && 600 > $this->curlInfo['http_code']);
            }
 
            public function getCurlInfo()
